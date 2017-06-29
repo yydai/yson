6211a68125f94ba0f493176d3739f930090ee987
Ying Dai <yingdai123@gmail.com>
2017-06-29 11:18:10 +0800

    update readme file

--- __init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = '2.1.1'
-__author__ = 'Ying Dai'
-
-# from .decoder import JSONDecoder
-
-
-# def loads(s):
-#   cls = JSONDecoder
-#  cls.decode(s)
---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+# To use a consistent encoding
+
+setup(
+    name='yson',
+
+    version='0.1',
+    packages=['yson'],
+
+    # The project's main homepage.
+    url='https://github.com/yydai/yson',
+    author='yydai',
+    author_email='dai92817@icloud.com',
+
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords=['json', 'parser', 'demo']
+)
